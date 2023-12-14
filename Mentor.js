fb509f989e155ee1588a177ef627ed0a4c95e55f
meetarora <meet.arora2403@gmail.com>
2023-12-14 13:02:44 +0530

    deploy

---
+++ Mentor.js
@@ -0,0 +1,56 @@
+let bt=document.querySelector(".first");
+let amt=document.querySelector(".amount1");
+bt.addEventListener("mouseover",()=>{
+    amt.style='visibility:visible';
+})
+bt.addEventListener("mouseleave",()=>{
+    amt.style='visibility:hidden';
+})
+let bt2=document.querySelector(".second");
+let amt2=document.querySelector(".amount2");
+bt2.addEventListener("mouseover",()=>{
+    amt2.style='visibility:visible';
+})
+bt2.addEventListener("mouseleave",()=>{
+    amt2.style='visibility:hidden';
+})
+let bt3=document.querySelector(".third");
+let amt3=document.querySelector(".amount3");
+bt3.addEventListener("mouseover",()=>{
+    amt3.style='visibility:visible';
+})
+bt3.addEventListener("mouseleave",()=>{
+    amt3.style='visibility:hidden';
+})
+let bt4=document.querySelector(".fourth");
+let amt4=document.querySelector(".amount4");
+bt4.addEventListener("mouseover",()=>{
+    amt4.style='visibility:visible';
+})
+bt4.addEventListener("mouseleave",()=>{
+    amt4.style='visibility:hidden';
+})
+let bt5=document.querySelector(".fifth");
+let amt5=document.querySelector(".amount5");
+bt5.addEventListener("mouseover",()=>{
+    amt5.style='visibility:visible';
+})
+bt5.addEventListener("mouseleave",()=>{
+    amt5.style='visibility:hidden';
+})
+let bt6=document.querySelector(".sixth");
+let amt6=document.querySelector(".amount6");
+bt6.addEventListener("mouseover",()=>{
+    amt6.style='visibility:visible';
+})
+bt6.addEventListener("mouseleave",()=>{
+    amt6.style='visibility:hidden';
+})
+let bt7=document.querySelector(".seventh");
+let amt7=document.querySelector(".amount7");
+bt7.addEventListener("mouseover",()=>{
+    amt7.style='visibility:visible';
+})
+bt7.addEventListener("mouseleave",()=>{
+    amt7.style='visibility:hidden';
+})
